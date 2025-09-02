96a315d3cdbc288962cc1b17c527b4547f448373
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-09-02 14:33:01 +0700

    Class on Pythone

--- python-class.py
+++ python-class.py
@@ -1,20 +1,13 @@
 class Hero:
-    pass
+    def __init__(self, inputName, inputPower, inputArmor, inputHealth):
+        self.name = inputName
+        self.power = inputPower
+        self.armor = inputArmor
+        self.health = inputHealth
 
-hero1 = Hero()
-hero2 = Hero()
-hero3 = Hero()
-
-hero1.name = "sniper"
-hero1.health = 100
-
-hero2.name = "sven"
-hero2.health = 200
-
-hero3.name = "thunder"
-hero3.health = 1000
-
-
-print(hero1)
+hero1 = Hero("sniper", 100,95,1000)
+hero2 = Hero("axe", 100,100,1000)
+hero3 = Hero("launcher", 100,80,1000)
 print(hero1.__dict__)
-print(hero1.name)
+print(hero2.__dict__)
+print(hero3.__dict__)
