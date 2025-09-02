95315ee02da951360445d529ed6fff214a5fa179
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-09-02 14:38:32 +0700

    Class Variable

--- python-class.py
+++ python-class.py
@@ -1,13 +1,19 @@
-class Hero:
+class Hero: #template
+    # Class Variabel
+    jumlah = 0
+
     def __init__(self, inputName, inputPower, inputArmor, inputHealth):
+        # Instance Variable
         self.name = inputName
         self.power = inputPower
         self.armor = inputArmor
         self.health = inputHealth
+        Hero.jumlah += 1
+        print("membuat hero dengan nama " + inputName)
 
 hero1 = Hero("sniper", 100,95,1000)
+print(Hero.jumlah)
 hero2 = Hero("axe", 100,100,1000)
+print(Hero.jumlah)
 hero3 = Hero("launcher", 100,80,1000)
-print(hero1.__dict__)
-print(hero2.__dict__)
-print(hero3.__dict__)
+print(Hero.jumlah)
